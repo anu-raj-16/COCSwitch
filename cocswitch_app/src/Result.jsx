e3f5d698b20962a1350c067a484ef36bcc668405
dshiang <dshiang@student.ubc.ca>
2025-01-19 01:08:40 -0800

    Adding html files

--- cocswitch_app/src/Result.jsx
+++ cocswitch_app/src/Result.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import NavigationBar from "./components/NavigationBar";
 import "./Result.css";
 
 function Result(props) {
+  const [result, setResult] = useState("");
+  if (props.result_med != -1) {
+    setResult("The medication to switch to is:" + props.result_med + ".");
+  } else {
+    setResult("There is currently no suitable COC replacement.");
+  }
+
   return (
     <>
       <NavigationBar />
       <div>
         <header>Form Result:</header>
         <div>
-          <body className="matched_result">
-            The medication to switch to is: {props.result}.
-          </body>
+          <body className="matched_result">{result}</body>
         </div>
       </div>
     </>
