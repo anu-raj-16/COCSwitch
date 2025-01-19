d2ed09171b127c957fd962bd31a2e6b4062f7a14
dshiang <dshiang@student.ubc.ca>
2025-01-19 02:59:14 -0800

    updated navbar and home and about pages

--- cocswitch_app/src/About.jsx
+++ cocswitch_app/src/About.jsx
@@ -1,4 +1,46 @@
+import "./About.css";
 function About() {
-  return <></>;
+  return (
+    <div className="flowchart_container">
+      <div>
+        <script
+          type="text/javascript"
+          src="https://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=3WOAJysoBtl4lQGs5iK3mOtCosV7xw4FilfbZUE-pqqzTdclAsTNNxNplQCk6_V3HQytEQEaOF3ec3WejR3mR67_yKFBK3lKUzG7UknqQwUVNgfnJ43uAVCnvz21cohF4Yj3hYJeWZONNi290SDBEHYFUzfCfiMfNhDI3brEl7FQhjbX8xvwVVRfGZqt2Ls-o2VQ4YHZOO_jP69_8IBuw3rcBPYB6sdrVKQau0PB8YlQKCOxWYvF95h6pGeJNClOjqmlogsbS2KD7ibbwHqzPA"
+        ></script>
+        <link
+          rel="stylesheet"
+          crossOrigin="anonymous"
+          href="https://gc.kis.v2.scr.kaspersky-labs.com/E3E8934C-235A-4B0E-825A-35A08381A191/abn/main.css?attr=aHR0cHM6Ly9jZG4uZGlzY29yZGFwcC5jb20vYXR0YWNobWVudHMvMTMyODYwODQ5NTY5OTc1OTExNC8xMzMwNDU3Njc1NzkxNzI4NjcwL2Zsb3cuaHRtbD9leD02NzhlMGNjNCZpcz02NzhjYmI0NCZobT03OTUwODExZDEyZjJlMzU5MzUyMjliMTg4MTA1NmM3Y2U4ZGYyMWM3OTViMzk0YzI3YzYzY2IxZTMzNjk0ZmZkJg"
+        />
+        <div className="title">
+          <h1>How to utilize our service</h1>
+        </div>
+        <div className="timeline">
+          <div className="checkpoint">
+            <div>
+              <h2>Select patient`s side effects</h2>
+              <p>Choose the symptoms the patient`s experiencing </p>
+            </div>
+          </div>
+          <div className="checkpoint">
+            <div>
+              <h2>Choose current COC</h2>
+              <p>
+                Input the medications the patient`s currently taking, including
+                name and dosage
+              </p>
+            </div>
+          </div>
+          <div className="checkpoint">
+            <div>
+              <h2>View results</h2>
+              <p>The given result is an apprpriate substitute medication</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style></style>
+    </div>
+  );
 }
 export default About;
--- cocswitch_app/src/Home.jsx
+++ cocswitch_app/src/Home.jsx
@@ -2,11 +2,58 @@ import "./Home.css";
 
 function Home() {
   return (
-    <>
-      <div>
-        <h1 className="title">COCSwitch</h1>
+    <div>
+      <section>
+        <div className="wave wave1"></div>
+        <div className="wave wave2"></div>
+        <div className="wave wave3"></div>
+        <div className="wave wave4"></div>
+      </section>
+      <div className="content" style={{ textAlign: "left" }}>
+        <div className="info">
+          <h2>
+            <em>
+              COC.switch: Empowering Pharmacists, Improving Women's Health
+            </em>
+          </h2>
+          <p>
+            At COC.switch, we understand that every woman’s health journey is
+            unique, <br />
+            and finding the right birth control option shouldn’t be a challenge.
+            Too often,
+            <br />
+            pharmacists face the daunting task of helping patients navigate the
+            maze of <br />
+            medications and symptoms, all while dealing with time constraints.
+            <br />
+            This is where we step in.
+            <br />
+            <br />
+            Our innovative tool is designed to support pharmacists in quickly
+            and accurately <br />
+            finding the best birth control alternatives for their patients. By
+            analyzing symptoms <br />
+            and medications, COC.switch delivers recommendations that are
+            tailored to the individual,
+            <br />
+            empowering pharmacists to provide better care and women to
+            experience fewer side effects.
+            <br />
+            <br />
+            With COC.switch, we’re not just streamlining a process, we’re
+            enhancing lives.
+            <br />
+            We believe that personalized, efficient healthcare should be
+            accessible to everyone,
+            <br />
+            and we’re committed to helping pharmacists provide the care that
+            every woman deserves.
+          </p>
+        </div>
       </div>
-    </>
+      <div className="img"></div>
+    </div>
   );
 }
+
 export default Home;
