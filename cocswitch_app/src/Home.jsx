e679fef0f3d2727c54a08715f4e6d009a4624d4a
dshiang <dshiang@student.ubc.ca>
2025-01-19 04:20:41 -0800

    Styled Home Page

--- cocswitch_app/src/Home.jsx
+++ cocswitch_app/src/Home.jsx
@@ -9,44 +9,32 @@ function Home() {
         <div className="wave wave3"></div>
         <div className="wave wave4"></div>
       </section>
-      <div className="content" style={{ textAlign: "left" }}>
-        <div className="info">
-          <h2>
-            <em>
-              COC.switch: Empowering Pharmacists, Improving Women's Health
-            </em>
+      <div className="content">
+        <div className="top_content">
+          <h1 className="project_title">Plan A</h1>
+          <h2 className="header_home">
+            <em>Empowering Clinicians, Improving Women's Health</em>
           </h2>
+        </div>
+        <div className="info">
           <p>
-            At COC.switch, we understand that every woman’s health journey is
-            unique, <br />
-            and finding the right birth control option shouldn’t be a challenge.
-            Too often,
-            <br />
-            pharmacists face the daunting task of helping patients navigate the
-            maze of <br />
-            medications and symptoms, all while dealing with time constraints.
-            <br />
-            This is where we step in.
-            <br />
-            <br />
-            Our innovative tool is designed to support pharmacists in quickly
-            and accurately <br />
-            finding the best birth control alternatives for their patients. By
-            analyzing symptoms <br />
-            and medications, COC.switch delivers recommendations that are
-            tailored to the individual,
-            <br />
-            empowering pharmacists to provide better care and women to
-            experience fewer side effects.
-            <br />
-            <br />
-            With COC.switch, we’re not just streamlining a process, we’re
-            enhancing lives.
-            <br />
-            We believe that personalized, efficient healthcare should be
-            accessible to everyone,
-            <br />
-            and we’re committed to helping pharmacists provide the care that
+            At Plan A, we understand that every woman’s health journey is
+            unique, and finding the right birth control option shouldn’t be a
+            challenge. Too often, clinicians face the daunting task of helping
+            patients navigate the maze of medications and symptoms, all while
+            dealing with time constraints.{" "}
+          </p>
+          <p>This is where we step in.</p>
+          <p>
+            Our innovative tool is designed to support clinicians in finding the
+            best birth control alternatives for their patients quickly and
+            accurately. By analyzing symptoms and current medications, Plan A
+            delivers recommendations that are tailored to the individual,
+            empowering clinicians to provide better care and enabling women to
+            experience fewer side effects. With Plan A, we’re not just
+            streamlining a process, we’re enhancing lives. We believe that
+            personalized, timely healthcare should be accessible to everyone,
+            and we’re committed to helping clinicians provide the care that
             every woman deserves.
           </p>
         </div>
